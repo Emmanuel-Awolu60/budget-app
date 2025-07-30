fd37e3cda98566a90148f279da8699234dc952cf
dev_eao <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2025-07-30 18:09:01 +0100

    Add Expense model, controller, and protected routes (add, get, delete)

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/expenses", expenseRoutes);
 
 // Root route
 app.get("/", (req, res) => {
